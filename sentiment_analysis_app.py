585be4389258a0390e7c597c5217aa18b8b69086
prabigya-pathak108 <risers273hari@gmail.com>
2024-05-25 18:58:45 +0545

    Codes arranged so that streamlit app can be deployed. Dependency resolved.

--- sentiment_analysis_app.py
+++ sentiment_analysis_app.py
@@ -6,7 +6,19 @@ import matplotlib.pyplot as plt
 import os
 #import sklearn
 
+import nltk
+import enchant   #for spelling correction and checking
+from nltk.metrics import edit_distance  # to find the case where spelling correction is needed
+
+from nltk.corpus import wordnet as wn,stopwords
+from nltk import pos_tag
+from nltk.stem import WordNetLemmatizer
+from collections import defaultdict
+
+import re
 current_dir = os.getcwd()
+d = enchant.Dict("en_US")
+
 
 
 # Construct the relative path to the file
@@ -16,9 +28,124 @@ current_dir = os.getcwd()
 # model_path_a = os.path.join(current_dir,r"code\saved_model_for_car_price.pkl")
 
 image_path = "sentiment.jpg"
+# Load the saved model
+with open('sentiment_analysis_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+# Load the CountVectorizer
+with open('cv.pkl', 'rb') as f:
+    cv = pickle.load(f)
+
+#Lets define some function for replacement of common sentece use cases
+replacement_patterns = [
+ (r'won\'t', 'will not'),
+ (r'can\'t', 'cannot'),
+ (r'i\'m', 'i am'),
+ (r'', ''),
+ (r'wanna', 'want'),
+ (r'gonna', 'going to'),
+ (r'ain\'t', 'is not'),
+ (r'(\w+)\'ll', '\g<1> will'),
+ (r'(\w+)n\'t', '\g<1> not'),
+ (r'(\w+)\'ve', '\g<1> have'),
+ (r'(\w+)\'s', '\g<1> is'),
+ (r'(\w+)\'re', '\g<1> are'),
+ (r'(\w+)\'d', '\g<1> would')
+]
+patterns = [(re.compile(regex), repl) for (regex, repl) in replacement_patterns]
+
+def replace_function(text):
+    s = text
+    for (pattern, repl) in patterns:
+        s = re.sub(pattern, repl, s)
+    return s
 
 
-options=[]
+
+
+#the following function remove stopwords
+def remove_stopwords(text):
+    stopwords_list=stopwords.words("english")
+    text_without_stopword=""
+    for i in str(text).split():
+        if i not in stopwords_list:
+            text_without_stopword=text_without_stopword+" "+str(i).lower()
+    return text_without_stopword.strip()
+
+
+
+
+#the following function is used for spelling checking and correction
+def correct_spellings_all(text):
+    words = text.split()
+    corrected_words = []
+    for word in words:
+        if d.check(word):
+            corrected_words.append(word)
+        else:
+            suggestions = d.suggest(word)
+            if suggestions:
+                if (edit_distance(word,suggestions[0])>1):
+                    corrected_words.append(suggestions[0])
+                else:
+                    corrected_words.append(word)
+            else:
+                corrected_words.append(word)
+    return ' '.join(corrected_words)
+
+
+
+
+#the follwing function is used for lammetizing by finding the POS
+tag_map = defaultdict(lambda : wn.NOUN)
+tag_map['J'] = wn.ADJ
+tag_map['V'] = wn.VERB
+tag_map['R'] = wn.ADV
+
+lemmatizer = WordNetLemmatizer()
+
+
+def lemmatize_sentence(text):
+    words = nltk.word_tokenize(text)
+    corrected_words = []
+    for token, tag in pos_tag(words):
+        lemma = lemmatizer.lemmatize(token, tag_map[tag[0]])
+        corrected_words.append(lemma)
+    return ' '.join(corrected_words)
+
+def preprocess_text(text):
+    # Apply preprocessing steps to the text
+    processed_text = replace_function(text)
+    processed_text = remove_stopwords(processed_text)
+    processed_text = correct_spellings_all(processed_text)
+    processed_text = lemmatize_sentence(processed_text)
+    return processed_text
+
+def expression_check(prediction_input):
+    if prediction_input == 0:
+        return "It has Negative Sentiment."
+    elif prediction_input == 1:
+        return "It has Positive Sentiment."
+    else:
+        return "Invalid Statement."
+
+def predict_from_user_input(user_input, model, cv):
+    # Preprocess the user input
+    processed_input = preprocess_text(user_input)
+    
+    # Transform the preprocessed input into numerical features
+    input_data = cv.transform([processed_input])
+    
+    # Make predictions using the trained model
+    predicted_class = model.predict(input_data)
+
+    predicted_probabilities = model.predict_proba(input_data)
+    predicted_class_index = np.argmax(predicted_probabilities)
+    #print(probability_predicted,confidence)
+    
+    prediction_msg =expression_check(predicted_class)
+
+    return predicted_probabilities,prediction_msg
 
 sentences = [
     "I am extremely happy with my new car; it drives smoothly and looks fantastic.",
@@ -60,18 +187,28 @@ with col2:
 
 
 if pred:
-    #prediction = model.predict(df1)[0]
-    st.success(f"Predicted Price of Your Car is : ₹", icon="✅")
+    if (user_input is None):
+        st.error("Please, Select At least One Sentence.",icon="📝")
+    else:
+        #prediction = model.predict(df1)[0]
+        probabilities,result = predict_from_user_input(user_input, model, cv)
+        st.text_area("Original Sentence", value=user_input, height=50)
+        
+        
+        if "Negative Sentiment." in result:
+            st.error(result,icon="🚨")
+        else:
+            st.success(result, icon="✅")
 
-    # Pie chart
-    labels = ['Confidence Level', 'Other']
-    sizes = [0.4, 0.6]
-    colors = ['#ff9999','#66b3ff']
-    explode = (0.1, 0)  # explode 1st slice
-    
-    fig1, ax1 = plt.subplots()
-    ax1.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%',
-            shadow=True, startangle=90)
-    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        # Pie chart
+        labels = ['Positive', 'Negative']
+        sizes = [probabilities[0][1], probabilities[0][0]]
+        colors = ['#ff9999','#66b3ff']
+        explode = (0.1, 0)  # explode 1st slice
+        
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%',
+                shadow=True, startangle=90)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-    st.pyplot(fig1)
+        st.pyplot(fig1)
